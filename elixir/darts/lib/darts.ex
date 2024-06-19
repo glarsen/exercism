dba47d3faaa6b713cf53e4df6df2360964ddcaea
Garrett Larsen <garrett.larsen@proton.me>
2024-06-19 17:50:20 -0600

    Solve Elixir - Darts
    Ref: https://exercism.org/tracks/elixir/exercises/darts

---
+++ elixir/darts/lib/darts.ex
@@ -0,0 +1,18 @@
+defmodule Darts do
+  @type position :: {number, number}
+
+  @doc """
+  Calculate the score of a single dart hitting a target
+  """
+  @spec score(position) :: integer
+  def score({x, y}) do
+    radius = (x * x + y * y) ** 0.5
+
+    cond do
+      radius <= 1  -> 10
+      radius <= 5  -> 5
+      radius <= 10 -> 1
+      true -> 0
+    end
+  end
+end
