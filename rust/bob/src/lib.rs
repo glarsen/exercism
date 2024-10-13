af108cc167b060b462eed78727d0b3a8e29db191
Garrett Larsen <garrett.larsen@proton.me>
2024-10-13 10:17:27 -0600

    Refactor Rust - Bob
    Ref: https://exercism.org/tracks/rust/exercises/bob

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -3,19 +3,18 @@ pub fn reply(message: &str) -> &str {
         .chars()
         .filter(|c| c.is_alphabetic() || c.is_ascii_punctuation())
         .collect();
+    
+    if message.is_empty() {
+        return "Fine. Be that way!"
+    }
 
-    let yelling = message.to_uppercase() == message && message.to_lowercase() != message;
+    let yelling = message.chars().any(|c| c.is_alphabetic()) && message.to_uppercase() == message;
     let question = message.ends_with('?');
 
-    if message.is_empty() {
-        "Fine. Be that way!"
-    } else if yelling && question {
-        "Calm down, I know what I'm doing!"
-    } else if question {
-        "Sure."
-    } else if yelling {
-        "Whoa, chill out!"
-    } else {
-        "Whatever."
+    match (yelling, question) {
+        (true, true) => "Calm down, I know what I'm doing!",
+        (true, false) => "Whoa, chill out!",
+        (false, true) => "Sure.",
+        (false, false) => "Whatever."
     }
 }
