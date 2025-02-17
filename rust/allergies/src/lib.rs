c9bf4ba528894ae0f99493b689a8e9a1171e6ca8
Garrett Larsen <garrett.larsen@proton.me>
2025-02-16 22:24:04 -0700

    Solve Rust - Allergies
    Ref: https://exercism.org/tracks/rust/exercises/allergies

---
+++ rust/allergies/src/lib.rs
@@ -0,0 +1,47 @@
+use crate::Allergen::{Cats, Chocolate, Eggs, Peanuts, Pollen, Shellfish, Strawberries, Tomatoes};
+use std::collections::HashMap;
+
+pub struct Allergies {
+    score: u32,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Allergen {
+    Eggs = 1,
+    Peanuts = 2,
+    Shellfish = 4,
+    Strawberries = 8,
+    Tomatoes = 16,
+    Chocolate = 32,
+    Pollen = 64,
+    Cats = 128,
+}
+
+static ALLERGENS: &'static [Allergen] = &[
+    Eggs,
+    Peanuts,
+    Shellfish,
+    Strawberries,
+    Tomatoes,
+    Chocolate,
+    Pollen,
+    Cats,
+];
+
+impl Allergies {
+    pub fn new(score: u32) -> Self {
+        Allergies { score }
+    }
+
+    pub fn is_allergic_to(&self, allergen: &Allergen) -> bool {
+        self.score & *allergen as u32 != 0
+    }
+
+    pub fn allergies(&self) -> Vec<Allergen> {
+        ALLERGENS
+            .iter()
+            .filter(|a| self.is_allergic_to(a))
+            .cloned()
+            .collect()
+    }
+}
